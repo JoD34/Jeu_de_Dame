ad80a1c6fd6fc9bae77a1b1c55c9062291d35a59
Joseph Descarreaux <joseph.descarreaux@outlook.com>
2023-12-24 12:15:20 -0500

    completion of attributs related methods

--- Jeton.py
+++ Jeton.py
@@ -1,26 +1,19 @@
 
 class Jeton:
-    def __init__(self, type) -> None:
+    def __init__(self, x, y, type = "pion") -> None:
         self.type = type
-        self.move_x = None
-        self.move_y = None
+        self.move = None
         self.color = None
+        self.x = x
+        self.y = y
     
-    def get_move_x(self):
+    def get_move(self):
         """Get the possible moves in x
 
         Returns:
             list: All possible moves in x
         """
-        return self.move_x
-    
-    def get_move_y(self):
-        """Get the possible moves in y
-
-        Returns:
-            list: All possible moves in y
-        """
-        return self.move_y
+        return self.move
     
     def get_color(self):
         """Get color of the pieces
@@ -38,6 +31,22 @@ class Jeton:
         """
         return self.type
     
+    def get_x(self):
+        """Get the position in x of the token
+
+        Returns:
+            int: x value of the position of the token
+        """
+        return self.x
+    
+    def get_y(self):
+        """Get the position in y of the token
+
+        Returns:
+            int: y value of the position of the token
+        """
+        return self.y
+        
     def set_type(self, new_type):
         """Set type of piece
 
@@ -46,21 +55,13 @@ class Jeton:
         """
         self.type = new_type
         
-    def set_move_x(self, new_move):
+    def set_move(self, new_move):
         """Set possible moves in x
 
         Args:
             new_move (list): all possible moves in x
         """
-        self.move_x = new_move
-        
-    def set_move_y(self, new_move):
-        """Set possible moves in y
-
-        Args:
-            new_move (list): all possible moves in y
-        """
-        self.move_y = new_move
+        self.move = new_move
     
     def set_color(self, color):
         """Set color of piece
@@ -69,3 +70,26 @@ class Jeton:
             color (str): color code of piece
         """
         self.color = color
+        
+    def set_x(self, new_x):
+        """Set new value of x
+
+        Args:
+            new_x (int): new value of the token on the x axis
+        """
+        self.x = new_x
+        
+    def set_y(self, new_y):
+        """Set new value of y
+
+        Args:
+            new_y (int): new value of the token on the y axis
+        """
+        self.y = new_y    
+    
+    def get_valid_moves(self):
+        return([(i, i) for i in self.move])
+    
+    def see_available_takes(self, board):
+        
+        
