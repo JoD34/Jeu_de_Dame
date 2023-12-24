024fe1241e3009c08d03f912ddb0462715b65a66
Joseph Descarreaux <joseph.descarreaux@outlook.com>
2023-12-24 01:11:48 -0500

    Premier commit de Damier

---
+++ Damier.py
@@ -0,0 +1,44 @@
+import Case
+
+class Damier():
+    def __init__(self) -> None:
+        squares = None
+        colors = {pale : "#eab676", fonce : "#1e81b0"}
+        
+    def get_square(self, x, y):
+        """Access a given square on the board.
+
+        Args:
+            x (int): position in x on the board.
+            y (int): position in y on the board.
+
+        Returns:
+            Case: case on the board.
+        """
+        return self.cases[x, y]
+    
+    def get_colors(self):
+        """Return the colors for the board
+
+        Returns:
+            dict: different colors of the board
+        """
+        return self.colors
+    
+    def create_square(self, x, y):
+        """Create a square
+
+        Args:
+            x (int): position in x on the board.
+            y (int): position in y on the board.
+            color (str): set color of the square.
+        """
+        color = self.get_colors()["pale" if ((x + y) % 2 == 0) else "fonce"]
+        return Case(pos_x = x, pos_y = y, color = color)
+    
+    def create_board(self):
+        """Create the hole board with the squares
+        """
+        SIDE = 10
+        self.square = [self.create_square(x = i, y = j) for j in range(SIDE) for i in range(SIDE)]
+                
