db82d0aa271454854dc3c30c7b5b4c626945e52a
Joseph Descarreaux <joseph.descarreaux@outlook.com>
2023-12-25 09:53:19 -0500

    Correction of order of attributes

--- Equipe.py
+++ Equipe.py
@@ -2,9 +2,10 @@ import Pion
 
 class Equipe():
     def __init__(self, color) -> None:
-        self.pion = self.make_team()
         self.image = ""
         self.color = color
+        self.color_code = {'black' : "#000000", 'white': "#FFFFFF"}
+        self.pion = self.make_team()
     
     def make_team(self):
         """Generate the full team with accurate coordonates
@@ -17,12 +18,13 @@ class Equipe():
         END_OF_BOARD = 10
         
         # Changes of coordonates for generating black teams
-        if (self.color == "Black"):
+        if (self.color == "black"):
             return [Pion(x = i, y = (END_OF_BOARD - j), color = self.color) 
                     for j in range(INIT_X) for i in range(INIT_Y)]
 
         # Generate White teams
-        return [Pion(x = i, y = j, color = self.color) for j in range(INIT_X) for i in range(INIT_Y)]
+        return [Pion(x = i, y = j, color = self.color) 
+                for j in range(INIT_X) for i in range(INIT_Y)]
     
     def get_pion(self):
         """Get list of all pions of a team
