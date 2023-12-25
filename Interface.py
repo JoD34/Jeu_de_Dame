ebd8fb4099469cb83770a5afa78168e96a2f8d90
Joseph Descarreaux <joseph.descarreaux@outlook.com>
2023-12-25 10:55:37 -0500

    Correction for board

--- Interface.py
+++ Interface.py
@@ -6,43 +6,38 @@ class JeuDeDame(Tk):
     def __init__(self, titre):
         super().__init__()
         self.title(titre)
-        self.side = 500
+        self.side = 750
         self.main_frame = None
-        self.teams = {"white": Equipe("#FFFFFF"), "noir": Equipe("#000000")}
+        self.teams = {"white": Equipe("white"), "noir": Equipe("black")}
         self.board = self.make_board()
         self.damier = Damier()
-        self.make_board()
         self.resizable(width = False, height = False) 
+        self.center_ecran()
     
     def center_ecran(self):
         """Centrer la fenêtre au centre de l'écran
         """
         # Calculate appropriate width
-        position_droite = int((self.winfo_screenwidth() - self.winfo_reqwidth()) / 2)
+        position_droite = int((self.winfo_screenwidth() - self.side) / 2)
         
         # Calculate appropriate height
-        position_bas = int((self.winfo_screenheight() - self.winfo_reqheight()) / 2)
-        
+        position_bas = int((self.winfo_screenheight() - self.side) / 2) + 100        
         # Applied geometry
         self.geometry(f"+{position_droite}+{position_bas}")
     
     def make_board(self):
         NUMBER_OF_SQUARES = 10
+        SIDE_SQUARE = self.side / NUMBER_OF_SQUARES
         # Make Frame
-        self.main_frame = Frame(master = self, background =  "#FFFFFF")
+        self.main_frame = Frame(master = self, background =  "#FFFFFF", width = self.side, height = self.side)
         
         for i in range(NUMBER_OF_SQUARES):
             for j in range(NUMBER_OF_SQUARES):
-                Frame(master = self.main_frame, background="#000")
-        # Make Canvas
-        #self.canvas = Canvas(master = self.main_frame, 
-        #                     width = self.side, height = self.side)
-        
-        # Draw squares
-        #for i in range(NUMBER_OF_SQUARES):
-        #    for j in range(NUMBER_OF_SQUARES):
-        #        color = Damier().get_colors()
-        #        self.canvas.create_rectangle(x0 = i, x1 = i * 50,
-        #                                     y0 = j, y1 = j * 50,
-        #                                     fill = color['pale' if((i + j) % 2 != 0) else 'fonce'])
-        self.center_ecran()
+                frame = Frame(master = self.main_frame, 
+                              background="#FFFFFF",
+                              width = SIDE_SQUARE, 
+                              height = SIDE_SQUARE,
+                              highlightbackground = "#000000", 
+                              highlightthickness = 0.5)
+                frame.grid(row = i, column = j)
+        self.main_frame.pack()
