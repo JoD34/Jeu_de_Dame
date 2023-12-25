d0af1b2cad9f6a5efce0ea3c17e37fc52ec8b441
Joseph Descarreaux <joseph.descarreaux@outlook.com>
2023-12-25 09:36:11 -0500

    Correction de l'héritage

--- Dame.py
+++ Dame.py
@@ -1,5 +1,6 @@
 import Jeton
 
 class Dame(Jeton):
-    def __init__(self) -> None:
-        move_x:[0, 10]
+    def __init__(self, x, y, color) -> None:
+        super().__init__(x = x, y = y, type = "Dame", color = color)
+        self.move:[0, 10]
