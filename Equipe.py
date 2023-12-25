ce5880419aae43e9e8a57c353a8f96241dacc957
Joseph Descarreaux <joseph.descarreaux@outlook.com>
2023-12-25 09:46:17 -0500

    add precision for generating teams if it is black or white

--- Equipe.py
+++ Equipe.py
@@ -3,13 +3,26 @@ import Pion
 class Equipe():
     def __init__(self, color) -> None:
         self.pion = self.make_team()
-        self.image = p
-        self.per_x = 5
-        self.per_y = 4
-        self.color = None
+        self.image = ""
+        self.color = color
     
     def make_team(self):
-        return [Pion(x = i,y = j, color = self.color) for j in self.per_x for i in self.per_y]
+        """Generate the full team with accurate coordonates
+
+        Returns:
+            list: Pions of one team
+        """
+        # Set variables
+        INIT_X, INIT_Y = 5, 4
+        END_OF_BOARD = 10
+        
+        # Changes of coordonates for generating black teams
+        if (self.color == "Black"):
+            return [Pion(x = i, y = (END_OF_BOARD - j), color = self.color) 
+                    for j in range(INIT_X) for i in range(INIT_Y)]
+
+        # Generate White teams
+        return [Pion(x = i,y = j, color = self.color) for j in range(INIT_X) for i in range(INIT_Y)]
     
     def get_pion(self):
         return self.pion
