f461fc0c2d25e237909f007735c6397ad5484793
Joseph Descarreaux <joseph.descarreaux@outlook.com>
2023-12-26 00:41:21 -0500

    Switch to canevas, add pions for red and black

--- Interface.py
+++ Interface.py
@@ -7,15 +7,19 @@ class JeuDeDame(Tk):
         super().__init__()
         self.title(titre)
         self.side = 650
-        self.main_frame = Frame(master = self, width = self.side, height = self.side)
+        self.number_of_squares = 10
         self.colors = {'pale' : "#edd2a7", 'fonce' : "#a24e31"}
-        self.teams = {"white": Equipe("white"), "red": Equipe("red")}
-        self.board = self.make_board()
+        self.board = []
+        self.turn = ['white', 'red']
+        
+        # Generate attributes with relation to other classes
         self.damier = Damier()
-        self.resizable(width = False, height = False) 
-        self.set_event()
+        self.main_frame = Frame(master = self, width = self.side, height = self.side)
+        self.teams = {"red": Equipe("red"), "black": Equipe("black")}
+        
+        # Miscellaneous functions
+        self.make_board()
         self.center_ecran()
-        print(self.teams["white"])
     
     def center_ecran(self):
         """Centrer la fenêtre au centre de l'écran
@@ -33,22 +37,38 @@ class JeuDeDame(Tk):
         """Generate all the frame for the board with coordinated colors.
         """
         # Constants
-        NUMBER_OF_SQUARES = 10
-        SIDE_SQUARE = self.side / NUMBER_OF_SQUARES
+        SIDE_SQUARE = self.side / self.number_of_squares
         
-        # Make frames for various squares
-        for i in range(NUMBER_OF_SQUARES):
-            for j in range(NUMBER_OF_SQUARES):
-                frame = Frame(master = self.main_frame, 
-                              background= self.colors["pale" if (i + j) % 2 == 0 else "fonce"],
-                              width = SIDE_SQUARE, 
-                              height = SIDE_SQUARE,
-                              highlightbackground = "#000000", 
-                              highlightthickness = 0.5)
-                frame.grid(row = i, column = j)
+        for i in range(self.number_of_squares):
+            for j in range(self.number_of_squares):
+                
+                # Create frame
+                canvas = Canvas(self.main_frame, 
+                                width=SIDE_SQUARE, 
+                                height=SIDE_SQUARE, 
+                                highlightthickness=0,
+                                background=self.colors["pale" if (i + j) % 2 == 0 else "fonce"])
+
+                
+                # Place frame on grid
+                canvas.grid(row = i, column = j)
+                
+                # Append frame to all frames
+                self.board.append(canvas)
         
         # Set every frame to viewers
         self.main_frame.pack()
+        
+        self.__set_pion_beginning("red")
+        self.__set_pion_beginning("black")
+        
+        # Enable resize
+        self.resizable(width = False, height = False) 
+   
+    def next_turn(self):
+        """Switch turn order keeper
+        """
+        self.turn = self.turn[-1: ]
         
     def click(self, event = None):
         """Get info of mouse click.
@@ -58,8 +78,29 @@ class JeuDeDame(Tk):
         """
         infos = event.widget.grid_info()
         print("row:", infos["row"], "column:", infos["column"])
-        
-    def set_event(self):
-        """Set every events objects.
+    
+    def __set_pion_beginning(self, team_color):
+        """Set pions for a team for the beginning of play
+
+        Args:
+            team_color (str): name of the team color
         """
-        self.bind("<Button-1>", self.click)
+
+        img = Equipe.get_images(team_color=team_color, piece_category='reg')
+        length = self.side / self.number_of_squares
+        beg_x, end_x = (0, 4) if team_color == 'black' else (6, 10)
+        
+        for i in range(beg_x, end_x):
+            for j in range(0, 10):
+                if (i + j) % 2 == 0 : continue
+                
+                canvas = self.board[int(str(i) + str(j))]
+
+                # Create an image item on the Canvas
+                canvas.create_image(length/2, length/2, anchor=CENTER, image=img)
+
+                # Bind the click event to the Canvas
+                canvas.bind("<Button-1>", self.click)
+
+                # Store the image reference in the frame (optional but can be useful)
+                canvas.image = img
