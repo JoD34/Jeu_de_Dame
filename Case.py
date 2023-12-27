9027655da2056c526c43fe42b816e3b633e8e0fe
Joseph Descarreaux <joseph.descarreaux@outlook.com>
2023-12-27 15:15:52 -0500

    Remove color relation to the case; Add jeton relation

--- Case.py
+++ Case.py
@@ -3,8 +3,7 @@ class Case:
     def __init__(self, x, y) -> None:
         self.x = x
         self.y = y
-        self.color = ""
-        self.occupied = False
+        self.jeton = None
     
     def get_x(self):
         """Get position of x value.
@@ -22,22 +21,13 @@ class Case:
         """
         return self.y
     
-    def get_color(self):
-        """Get color of pion placed on square
-
-        Returns:
-            str: team color of the pion placed on the square.
-                Return None if no pion is places on the square  
-        """
-        return self.color
-    
     def get_occupancy(self):
         """Get occupancy statue of the square.
 
         Returns:
             bool: True if occupied; False if not.
         """
-        return self.occupied
+        return self.jeton is None
     
     def set_x(self, new_x):
         """Set new position in x.
@@ -55,15 +45,10 @@ class Case:
         """
         self.y = new_y
         
-    def set_color(self, new_color):
-        """Set a new color for the team occupying a square
+    def set_jeton(self, jeton):
+        """Set a jeton on the square
 
         Args:
-            new_color (str): team color occupying new square
+            jeton (Jeton): Jeton place on the Case. Can be an object of type Pion or Dame
         """
-        self.color = new_color
-        
-    def switch_occupancy(self):
-        """Switch status of occupancy.
-        """
-        self.occupied = not self.get_occupancy()
+        self.jeton = jeton
