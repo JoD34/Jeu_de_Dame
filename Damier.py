35c09728c384baa8a6b14a469ff005df7af52aff
Joseph Descarreaux <joseph.descarreaux@outlook.com>
2023-12-26 20:37:07 -0500

    Correction for highlighting: Check if the square is friend or foe

--- Damier.py
+++ Damier.py
@@ -27,14 +27,15 @@ class Damier():
         Returns:
             dict: squares retrieved
         """
+        # Get current square
+        square = self.squares[x][y]
+        
         # Correction depending of color
         x  = x + 1 if team_color == 'black' else x - 1
         
         # Get squares that should be available
-        left = self.get_square(x=x, y=(y - 1)) if ((y - 1  < 10) and (y - 1 >= 0)) else None
-        right = self.get_square(x=x, y=(y + 1)) if ((y + 1  < 10) and (y + 1 >= 0)) else None
-        
-        return {'left' : left, 'right' : right}
+        return {'left' : self.__set_highlight_square(x=x, y=(y - 1), current=square),
+                'right' : self.__set_highlight_square(x=x, y=(y + 1), current=square)}
     
     def create_square(self, x, y):
         """Create a square
@@ -53,4 +54,26 @@ class Damier():
         SIDE = 10
         return [[self.create_square(x=i, y=j) for j in range(SIDE)] for i in range(SIDE)]
     
+    def __check_if_friend(self, current, square):
+        """Check if it is friend or foe
+
+        Args:
+            current (Case): Current Case. Origin of the event
+            square (Case): diagonal Case. Checked if foe or not
+
+        Returns:
+            bool: True if friend. False if foe.
+        """
+        return current.get_color() == square.get_color()
+        
+    def __set_highlight_square(self, x, y, current):
+        
+        # Check if column index is out of bound
+        if ((y >= 10) and (y < 0)): return None
+        
+        # Get square
+        square = self.get_square(x=x, y=y) 
+
+        # Return if square should be highlighted
+        return None if (self.__check_if_friend(current=current, square=square)) else square
                 
