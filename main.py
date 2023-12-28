be7e82682efc0e76c793a1442ae294b46610974c
Joseph Descarreaux <joseph.descarreaux@outlook.com>
2023-12-28 15:27:20 -0500

    Add comments

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from Interface import JeuDeDame
-from tkinter import mainloop
+from tkinter import mainloop    
 
+# Generate main object for the interface and the project
 root = JeuDeDame('Jeu de Dames')
+
+# Generate main loop to run the game
 root.mainloop()
