2f0b9f82d65ff506c51cd86d328d1e0400f9ef1a
Joseph Descarreaux <joseph.descarreaux@outlook.com>
2023-12-31 11:34:09 -0500

    add attribut relating to movement

--- Pion.py
+++ Pion.py
@@ -4,3 +4,4 @@ from Jeton import Jeton
 class Pion(Jeton):
     def __init__(self, x, y, case, color) -> None:
         super().__init__(x=x, y=y, type="pion", case=case, color=color)
+        self.direction = 'up' if color == 'red' else 'down'
