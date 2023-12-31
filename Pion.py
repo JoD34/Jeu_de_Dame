5eed0909fc242361874b5a42128aff6608e56f79
Joseph Descarreaux <joseph.descarreaux@outlook.com>
2023-12-31 10:55:27 -0500

    Removing unnecessary methods in the class pion

--- Pion.py
+++ Pion.py
@@ -3,12 +3,3 @@ from Jeton import Jeton
 class Pion(Jeton):
     def __init__(self, x, y, case, color) -> None:
         super().__init__(x = x, y = y, type = "pion", case = case, color = color)
-    
-    def to_queen(self, board):
-        pass
-        
-    
-    def see_available_takes(self, board):
-        left = board.get_squares()[self.x - 1, self.y + 1]
-        right = board.get_squares()[self.x + 1, self.y + 1]
-        if not (left.occupied() or right.occupied()): return(None)
