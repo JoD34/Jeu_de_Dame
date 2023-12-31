4bbb068afb189b712ca30c13c4441b6433cae34a
Joseph Descarreaux <joseph.descarreaux@outlook.com>
2023-12-31 10:55:00 -0500

    Adding a attribut to initialize the Dame in accordance with the master class Jeton

--- Dame.py
+++ Dame.py
@@ -1,6 +1,6 @@
 import Jeton
 
 class Dame(Jeton):
-    def __init__(self, x, y, color) -> None:
-        super().__init__(x = x, y = y, type = "Dame", color = color)
-        self.move:[0, 10]
+    def __init__(self, x, y, case, color) -> None:
+        super().__init__(x=x, y=y, type="Dame", case=case, color=color)
+        self.move = [-10, 10]
