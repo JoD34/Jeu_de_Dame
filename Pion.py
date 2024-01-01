f0b8c778beafd42e55b19f8879cf02a8f7a88ebe
Joseph Descarreaux <joseph.descarreaux@outlook.com>
2024-01-01 12:15:35 -0500

    Added a method to get de direction of pions

--- Pion.py
+++ Pion.py
@@ -5,3 +5,10 @@ class Pion(Jeton):
     def __init__(self, x, y, case, color) -> None:
         super().__init__(x=x, y=y, type="pion", case=case, color=color)
         self.direction = 'up' if color == 'red' else 'down'
+
+    def get_direction(self):
+        """
+        Get the direction of the pion object
+        :return: the direction of the pion object
+        """
+        return self.direction
